1c04a27f504efdf0063d10c8b8b7c6ede4e0ad17
Amine ElQaraoui <aelqaraoui101@gmail.com>
2023-04-09 02:30:59 +0000

    added analytics

--- src/App.js
+++ src/App.js
@@ -7,24 +7,26 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Profile from './components/Profile';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   const [requiresLogin, setRequiresLogin] = useState(true);
 
   return (
     <Router>
-        <div className="app">
-        <Sidebar />
-        <div className="main-content">
-            <Navbar requiresLogin={requiresLogin} setRequiresLogin={setRequiresLogin} />
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/" element={<FeedApp requiresLogin={requiresLogin} setRequiresLogin={setRequiresLogin} />} />
-                <Route path="/profile" element={<Profile />} />
-            </Routes>
-        </div>
-        </div>
+      <Analytics />
+      <div className="app">
+      <Sidebar />
+      <div className="main-content">
+          <Navbar requiresLogin={requiresLogin} setRequiresLogin={setRequiresLogin} />
+          <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/" element={<FeedApp requiresLogin={requiresLogin} setRequiresLogin={setRequiresLogin} />} />
+              <Route path="/profile" element={<Profile />} />
+          </Routes>
+      </div>
+      </div>
     </Router>
   );
 };
